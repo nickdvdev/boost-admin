18c2e3c08e48bfead1092c20a85be2e2c32eb991
nickdvdev <nickdv.dev@gmail.com>
2020-08-29 14:32:32 +0300

    fix helpers import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const helpers = require('helpers.js')
+const helpers = require('./helpers')
 const markup = require('telegraf/markup') 
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
