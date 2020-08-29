44f866d92c2f85e750d3cfd098983de5ee12b4bc
nickdvdev <nickdv.dev@gmail.com>
2020-08-29 15:21:37 +0300

    newcomers fix

--- src/index.js
+++ src/index.js
@@ -2,27 +2,47 @@ require('dotenv').config();
 const helpers = require('./helpers')
 const markup = require('telegraf/markup') 
 const { Telegraf } = require('telegraf')
+const Telegram = require('telegraf/telegram')
+const telegram = new Telegram(process.env.BOT_TOKEN)
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const adminsChat = process.env.ADMINS_CHAT
+const ordersChat = process.env.ORDERS_CHAT
 bot.launch()
 helpers.logStart()
 //======================================================
 
+// bot.on('left', (ctx) => {
+//   if (ctx.chat.id === ordersChat) {
+//     const newMemberText = `Добавился в чат Заказы
+//     ===================
+//     USER INFO
+//     Имя: ${ctx.from.first_name}
+//     Username: @${ctx.from.username}
+//     Язык: ${ctx.from.language_code}`
+//     telegram.sendMessage(adminsChat, newMemberText)
+//   }
+//   return
+// })
+
+
 bot.on('new_chat_members', (ctx) => {
-  const newMemberText = `Добавился в чат Заказы
+  const newMemberText = `Пришел в чат Заказы
 ===================
 USER INFO
 Имя: ${ctx.from.first_name}
 Username: @${ctx.from.username}
 Язык: ${ctx.from.language_code}`
-  bot.sendMessage(ADMIN_CHAT, newMemberText, ctx)
+    telegram.sendMessage(adminsChat, newMemberText)
+  return
 })
 
 bot.on('left_chat_member', (ctx) => {
   const newMemberText = `Ушел из чата Заказы
 ===================
 USER INFO
-Имя: ${helpers.getName(ctx)}
+Имя: ${ctx.from.first_name}
 Username: @${ctx.from.username}
 Язык: ${ctx.from.language_code}`
-  bot.sendMessage(ADMIN_CHAT, newMemberText, ctx)
+  telegram.sendMessage(adminsChat, newMemberText)
+  return
 })
