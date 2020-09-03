37259e4c6821fe9cca3888ad0985c1429f3349fa
nickdvdev <nickdv.dev@gmail.com>
2020-09-03 17:14:44 +0300

    another try docs fix

--- src/index.js
+++ src/index.js
@@ -42,6 +42,6 @@ bot.on('message', (ctx) => {
 
 bot.command('docs', (ctx) => {
   if (ctx.message.chat.id === adminsChat) {
-      telegram.sendMessage(adminsChat, docsLink)
+      ctx.reply(docsLink)
   }
 })
